7fc4ad5062397de32d6f90b4d9d339d23db88803
LAPTOP\hyq <hyq961206@sjtu.edu.cn>
2021-05-07 04:19:33 +0900

    go——how to extend a package alias, composition & embedding

---
+++ queue/entry/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"goLearning/queue"
+)
+
+func main() {
+	q := queue.Queue{1}
+
+	q.Push(2)
+	q.Push(3)
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+
+	//问：q := queue.Queue{1}的q和最后fmt.Println(q.IsEmpty())的q是同一个q，同一个slice吗？
+	//答：不是哦~GO语言比较特殊，
+	//push和pop可以改变这个q的值，因为push和pop定义中的接收者是指针接收者。
+	//所以可以改变里面的值，即他把q改了
+}
--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -22,9 +22,9 @@ func main() {
 	//值接受者是GO特有的，别的语言莫得，别的只有指针接收者
 	//值/指针接收者可以接受值/指针都ok，所以定义方法的人可以随便改接收者为值or指针接收者，不影响调用者
 
-	var root tree.Node
-
-	root = tree.Node{Value: 3}
+	//var root tree.Node
+	//root = tree.Node{Value: 3}
+	root := myTreeNode{&tree.Node{Value: 3}}
 	root.Left = &tree.Node{}
 	root.Right = &tree.Node{5, nil, nil}
 	root.Right.Left = new(tree.Node) // no more -> like root.right->left, all are .
@@ -33,16 +33,20 @@ func main() {
 	root.Traverse()
 	fmt.Println()
 
-	root.Right.Left.Print()
+	//myRoot := myTreeNode{&root}
+	//myRoot.postOrder()
+	root.postOrder()
 
-	root.Print()
-	root.SetValue(100)
-	pRoot := &root
-	pRoot.Print()
-	pRoot.SetValue(200)
-	pRoot.Print()
-	fmt.Println()
-	root.Traverse()
+	//root.Right.Left.Print()
+	//
+	//root.Print()
+	//root.SetValue(100)
+	//pRoot := &root
+	//pRoot.Print()
+	//pRoot.SetValue(200)
+	//pRoot.Print()
+	//fmt.Println()
+	//root.Traverse()
 	// we can also create a slice
 	//nodes := []TreeNode{
 	//	{value: 4},
@@ -50,4 +54,29 @@ func main() {
 	//	{5, nil, &root},
 	//}
 	//fmt.Println(nodes)
+}
+
+//GO无继承，如何扩展别人写的包呢？3种办法
+//1. 别名法（见container中的queue和sliceops）该办法最简单，但若之后需要过渡到组合法改起来很麻烦
+//2. 组合法 最常见 除非确保以后不动了的代码用别名法更快乐，其余情况还是组合吧
+//2.1 内嵌法 Embedding 特殊的语法糖 好处是能少写许多代码 但要注意 这种写法得大家都是熟练工的时候这么写，否则会让萌新们感到混乱的
+//组合法把组合的结构体的名字去掉，就留个类型，就成内嵌了
+//那没了名字，我们怎么找到它呢？GO会给他们起个名字，名字就是类型最后一个.后边的东西， 此处*tree.Node，即为Node
+//然后此时还有个语法糖，访问外头那层直接可以.到里头包的（成员变量&方法都有）
+//这玩意儿和继承很像呢!但它不是哦~本质还是组合（子类指针指向父类，可以发现指不过去，真想这么干，用接口!）
+//那 有重载吗？确实类似，定义一个同名的函数，则调用xx.原来函数()时，原来的会被新的覆盖掉（shadowed），但你仍可以通过xx.Node.原来函数()来访问原来的
+type myTreeNode struct {
+	*tree.Node //此即内嵌，原为 node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.Node == nil {
+		return
+	}
+
+	left := myTreeNode{myNode.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.Right}
+	right.postOrder()
+	myNode.Print()
 }
