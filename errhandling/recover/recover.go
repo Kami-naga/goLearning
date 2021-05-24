3e9b81e07f83ac805e63ba11f4a17971eac1221f
LAPTOP\hyq <hyq961206@sjtu.edu.cn>
2021-05-25 00:54:02 +0900

    go——panic & recover

---
+++ errhandling/recover/recover.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	tryRecover()
+}
+
+func tryRecover() {
+	defer func() {
+		r := recover() //recover returns an interface! (any type) &recover can only be used in defer
+		if err, ok := r.(error); ok {
+			fmt.Println("Error occurred:", err)
+		} else {
+			panic(fmt.Sprintf("I don't know what to do with: %v", r))
+		}
+	}() // we need the () here,
+	// which means we called a function we created(definition+ function call)
+	// just like defer func()
+
+	//panic(errors.New("this is an error"))
+
+	//b := 0
+	//a := 5 / b
+	//fmt.Println(a)
+
+	panic(123)
+}
