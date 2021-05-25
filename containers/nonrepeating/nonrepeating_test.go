0e9030f4a1c293963f6daeacdd99d2026165f3f1
LAPTOP\hyq <hyq961206@sjtu.edu.cn>
2021-05-26 00:48:20 +0900

    go——performance optimization

--- containers/nonrepeating/nonrepeating_test.go
+++ containers/nonrepeating/nonrepeating_test.go
@@ -2,6 +2,9 @@ package main
 
 import "testing"
 
+//use go test -coverprofile=c.out
+//to get a coverage profile file &
+//use go tool cover -html=c.out to see the result(use cmd,then you can get a html)
 func TestSubstr(t *testing.T) {
 	tests := []struct {
 		s   string
@@ -26,6 +29,33 @@ func TestSubstr(t *testing.T) {
 		if actual != tt.ans {
 			t.Errorf("got %d for input %s; "+
 				"expected %d", actual, tt.s, tt.ans)
+		}
+	}
+}
+
+//we can also do below tests by cmd
+// go test -bench .
+//we can also use
+//go test -bench . -cpuprofile cpu.out
+//to get a performance file(binary), then use
+//go tool pprof cpu.out            to see the result
+//in pprof, type web to get the graph(graphviz needed)
+func BenchmarkSubstr(b *testing.B) {
+	s, ans := "今天天气不错，我们去散步吧", 12
+	for i := 0; i < 13; i++ {
+		s = s + s
+	}
+	b.Logf("len(s) = %d", len(s))
+
+	b.ResetTimer() //just count time of below codes
+
+	// how many times we need to run below codes?
+	// b.N times! benchmark decides that
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ans {
+			b.Errorf("got %d for input %s; "+
+				"expected %d", actual, s, ans)
 		}
 	}
 }
--- containers/nonrepeating/norepeating.go
+++ containers/nonrepeating/norepeating.go
@@ -12,19 +12,36 @@ func main() {
 	fmt.Println(lengthOfNonRepeatingSubStr("PPP今日は晴れだ!"))
 }
 
+//put slice making outside the function to avoid it making slice many times,
+//then we can get better performance in benchmark
+//but I just found the result is the same, so it maybe optimized
+var lastOccurred = make([]int, 0xffff) //65535 length, 'a' = 97d = 0x61, so lastOccurred['a'] = x is just like a map
+
+//form the result of pprof, we can get the most time-consuming part of the code is
+//1. decoding string to rune
+//2. map operation(map access & map assign)
+//so how to optimize? 1 is inevitable because we need rune to decode and understand the contents,
+//but we can optimize map(just use another data structure)
+//each time we add an element into a map,
+//map needs to calculate hash, check if duplicate, assign space if not enough...
+//so we can use more space to get better time, just make a large slice instead of a map!
+//lastOccurred := make(map[rune]int)
 func lengthOfNonRepeatingSubStr(s string) int {
-	lastOccured := make(map[rune]int)
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
 	start := 0
 	maxLength := 0
-
+	//rune(s) -> decoding& make a new rune array, not re-understanding the same memory,
 	for i, ch := range []rune(s) { //only range s will not run well because each jp/ch charac will add 3(length)
-		if lastI, ok := lastOccured[ch]; ok && lastI >= start {
-			start = lastOccured[ch] + 1
+		//if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
+			start = lastOccurred[ch] + 1
 		}
 		if i-start+1 > maxLength {
 			maxLength = i - start + 1
 		}
-		lastOccured[ch] = i
+		lastOccurred[ch] = i
 	}
 
 	return maxLength
